5f63f14170d03c4a04cca2a3e0593e85358b1529
Nikolay.Kostadinov <Kostadinov.Nikolay.d@gmail.com>
2025-08-19 13:32:13 +0300

    refactored user-management.service

--- client/src/app/features/admin/services/user-management.service.ts
+++ client/src/app/features/admin/services/user-management.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, DestroyRef, inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpResponse, HttpParams } from '@angular/common/http';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { catchError, map, tap, throwError, Observable } from 'rxjs';
+import { catchError, map, throwError, Observable } from 'rxjs';
 import { UserListModel } from '../../profile/models/user-list.model';
 import { UserProfileModel } from '../../profile/models';
-import { ApiError, PagedFilteredRequest, PagedResult } from '../../../common';
+import { PagedFilteredRequest, PagedResult } from '../../../common';
 import { environment } from '../../../../environments/environment';
 import { API_ENDPOINTS } from '../../../common/constants/api-endpoints';
 import { AuthHttpService } from '../../auth';
@@ -16,9 +15,6 @@ import { AuthHttpService } from '../../auth';
 export class UserManagementService {
     private readonly authHttpService = inject(AuthHttpService);
     private readonly http = inject(HttpClient);
-    private readonly destroyRef = inject(DestroyRef);
-
-    constructor() { }
 
     public getAllUsers(request: PagedFilteredRequest): Observable<HttpResponse<PagedResult<UserProfileModel>>> {
         let params = this.initializeQueryParams(request);
@@ -29,6 +25,30 @@ export class UserManagementService {
                 observe: 'response'
             }
         );
+    }
+
+    deleteProfile(userId: string): Observable<void> {
+        return this.authHttpService.deleteProfile(userId)
+            .pipe(
+                map(() => void 0),
+                catchError((error) => throwError(() => error))
+            );
+    }
+
+    toggleUserRole(user: UserListModel, role: string): Observable<void> {
+        if (this.userInRole(user, role)) {
+            return this.removeRoleFromUser(user.id, role)
+                .pipe(
+                    map(() => void 0),
+                    catchError((error) => throwError(() => error))
+                );
+        } else {
+            return this.addRoleToUser(user.id, role)
+                .pipe(
+                    map(() => void 0),
+                    catchError((error) => throwError(() => error))
+                );
+        }
     }
 
     private initializeQueryParams(request: PagedFilteredRequest): HttpParams {
@@ -50,64 +70,7 @@ export class UserManagementService {
         return params;
     }
 
-    public loadUsers(request: PagedFilteredRequest): Observable<PagedResult<UserListModel> | null> {
-        return this.getAllUsers(request)
-            .pipe(
-                takeUntilDestroyed(this.destroyRef),
-                map(response => response
-                    ? <PagedResult<UserListModel> | null>{
-                        ...response.body!,
-                        items: response.body!.items.map(user => {
-                            return ({
-                                ...user,
-                                rolesView: user.roles.length
-                                    ? user.roles.map(role => role.name).join(', ')
-                                    : '-'
-                            }) as UserListModel;
-                        })
-                    }
-                    : null),
-                catchError((error) => {
-                    console.error('Error loading user profiles:', error);
-                    return throwError(() => error);
-                })
-            );
-    }
-
-    deleteProfile(userId: string): Observable<void> {
-        return this.authHttpService.deleteProfile(userId)
-            .pipe(
-                map(() => void 0), // Return void
-                takeUntilDestroyed(this.destroyRef),
-                catchError((error) => {
-                    return throwError(() => error);
-                })
-            );
-    }
-
-    toggleUserRole(user: UserListModel, role: string): Observable<void> {
-        if (this.userInRole(user, role)) {
-            return this.removeRoleFromUser(user.id, role)
-                .pipe(
-                    map(() => void 0), // Return void
-                    takeUntilDestroyed(this.destroyRef),
-                    catchError((error) => {
-                        return throwError(() => error);
-                    })
-                );
-        } else {
-            return this.addRoleToUser(user.id, role)
-                .pipe(
-                    map(() => void 0), // Return void
-                    takeUntilDestroyed(this.destroyRef),
-                    catchError((error) => {
-                        return throwError(() => error);
-                    })
-                );
-        }
-    }
-
-    public addRoleToUser(id: string, role: string): Observable<HttpResponse<void>> {
+    private addRoleToUser(id: string, role: string): Observable<HttpResponse<void>> {
         return this.http.patch<void>(
             `${environment.baseUrl}${API_ENDPOINTS.ACCOUNT.ADD_TO_ROLE}/${id}/${role}`,
             {},
@@ -115,7 +78,7 @@ export class UserManagementService {
         );
     }
 
-    public removeRoleFromUser(id: string, role: string): Observable<HttpResponse<void>> {
+    private removeRoleFromUser(id: string, role: string): Observable<HttpResponse<void>> {
         return this.http.patch<void>(
             `${environment.baseUrl}${API_ENDPOINTS.ACCOUNT.REMOVE_FROM_ROLE}/${id}/${role}`,
             {},
